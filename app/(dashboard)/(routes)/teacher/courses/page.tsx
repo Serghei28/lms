e500f50bb517aa3a3d5732da3aeafb925cd69188
Serghei28 <serghei.petis@isa.utm.md>
2025-10-15 12:57:48 +0300

    Интерфейс создания курса

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const CoursesPage = () => {
     return ( 
         <div>
-            Courses Page
+            <Button className="p-6">
+                <Link href="/teacher/create">
+                    Courses Page
+                </Link>
+            </Button>
         </div>
      );
 }
