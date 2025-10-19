5f21e791b0fde65777fcbf6d00b698bba4ce27e5
Serghei28 <serghei.petis@isa.utm.md>
2025-10-19 12:36:58 +0300

    UploadThing и форма загрузки изображения

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "utfs.io",
+      "cdn.utfs.io"
+    ]
+  }
 };
 
 export default nextConfig;
