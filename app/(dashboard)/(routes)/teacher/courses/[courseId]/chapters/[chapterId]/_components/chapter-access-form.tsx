aa59107ecd16d3ed83aa9c64e35828300d197276
Serghei28 <serghei.petis@isa.utm.md>
2025-11-03 10:55:08 +0200

    Форма доступа к главе

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-access-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-access-form.tsx
@@ -8,6 +8,7 @@ import { useForm } from "react-hook-form";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormMessage,
@@ -21,22 +22,23 @@ import { cn } from "@/lib/utils";
 import { Chapter } from "@prisma/client";
 import { MyEditor } from "@/components/editor";
 import { Preview } from "@/components/preview";
+import { Checkbox } from "@/components/ui/checkbox";
 
-interface ChapterDescriptionFormProps {
+interface ChapterAccessFormProps {
   initialData: Chapter;
   courseId: string;
   chapterId: string;
 }
 
 const formSchema = z.object({
-  description: z.string().min(1),
+  isFree: z.boolean().default(false),
 });
 
-export const ChapterDescriptionForm = ({
+export const ChapterAccessForm = ({
   initialData,
   courseId,
     chapterId,
-}: ChapterDescriptionFormProps) => {
+}: ChapterAccessFormProps) => {
     const [isEditing, setIsEditing] = useState(false);
 
     const toggleEdit = () => setIsEditing((current) => !current);
@@ -44,9 +46,9 @@ export const ChapterDescriptionForm = ({
     const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+    resolver: zodResolver(formSchema) as any,
     defaultValues: {
-        description: initialData?.description || ""
+        isFree: initialData.isFree,
     },
   });
 
@@ -66,14 +68,14 @@ export const ChapterDescriptionForm = ({
     return (
         <div className="mt-6 border bg-slate-100 rounded-md p-4">
             <div className="font-medium flex items-center justify-between">
-            Chapter description
+            Chapter access
             <Button onClick={toggleEdit} variant="ghost">
                 {isEditing ? (
                     <>Cancel</>
                 ) : (
                     <>
                         <Pencil className="h-4 w-4 mr-2" />
-                        Edit description
+                        Edit access
                     </>
                 )}
             </Button>
@@ -81,14 +83,13 @@ export const ChapterDescriptionForm = ({
             {!isEditing && (
                 <div className={cn(
                     "text-sm mt-2",
-                    !initialData.description && "text-slate-500 italic"
+                    !initialData.isFree && "text-slate-500 italic"
                 )}>
-                    {!initialData.description && "No description."}
-                    {initialData.description && (
-                        <Preview 
-                        value={initialData.description}
-                        />
-                    )}
+                   {initialData.isFree ? (
+                    <>This chapter is free for preview.</>
+                   ) : (
+                    <>This chapter is not free</>
+                   )}
                 </div>
             )}
             {isEditing && (
@@ -99,16 +100,21 @@ export const ChapterDescriptionForm = ({
                     >
                         <FormField
                             control={form.control}
-                            name="description"
+                            name="isFree"
                             render={({ field }) => (
-                            <FormItem>
-                                <FormControl>
-                                    <MyEditor
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
+                                <FormItem className=" flex flex-row items-start space-x-3 space-y-0 rounded border p-4">
+                                    <FormControl>
+                                        <Checkbox 
+                                        checked={field.value}
+                                        onCheckedChange={field.onChange}
+                                        />
+                                    </FormControl>
+                                    <div className="space-y-1 leading-none">
+                                        <FormDescription>
+                                            Check this box if you want to make this chapter free for priwiew
+                                        </FormDescription>
+                                    </div>
+                                </FormItem>
                             )}
                         />
                         <div className="flex items-center gap-x-2">
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -3,10 +3,11 @@ import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
 import Link from "next/link";
-import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react";
 import { IconBadge } from "@/components/icon-badge";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
+import { ChapterAccessForm } from "./_components/chapter-access-form";
 
 const ChapterIdPage = async ({
   params
@@ -87,6 +88,27 @@ const ChapterIdPage = async ({
                     chapterId={params.chapterId}
                 />
             </div>
+            <div>
+              <div className="flex items-center gap-x-2">
+                  <IconBadge icon={Eye}/>
+                  <h2 className="text-xl">
+                    Acces Settings
+                  </h2>
+              </div>
+              <ChapterAccessForm 
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+              />
+            </div>
+        </div>
+        <div>
+          <div className=" flex items-center gap-x-2">
+            <IconBadge icon={Video} />
+          </div>
+          <h2 className="text-xl">
+            Add a Video
+          </h2>
         </div>
       </div>
     </div>
