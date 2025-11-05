13f3b1945834ccb30878f30ebbcbcde828f2766a
Serghei28 <serghei.petis@isa.utm.md>
2025-11-05 20:10:04 +0200

    Таблица данных курсов

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,32 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
 
-const CoursesPage = () => {
+import { db } from "@/lib/db";
+
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+
+
+const CoursesPage = async () => {
+    const { userId } = await auth();
+
+    if(!userId) {
+        return redirect("/");
+    }
+
+    const courses = await db.course.findMany({
+        where: {
+            userId,
+        },
+        orderBy: {
+            createdAt: "desc"
+        },
+    });
+
     return ( 
-        <div>
-            <Button className="p-6">
-                <Link href="/teacher/create">
-                    Courses Page
-                </Link>
-            </Button>
+        <div className="p-6">
+            <DataTable columns={columns} data={courses} />
         </div>
      );
 }
