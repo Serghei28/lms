de6ae1b70f33a7d791063ddbe22c6d9a8d8874f2
Serghei28 <serghei.petis@isa.utm.md>
2025-11-11 18:50:07 +0200

    Интеграция Stripe (оплата)

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
 
@@ -12,8 +16,27 @@ export const CourseEnrollButton = ({
 price,
 courseId,
 }: CourseEnrollButtonProps) => {
+    const [isLoading, setIsLoanidg] = useState(false);
+
+    const onClick = async () => {
+        try {
+            setIsLoanidg(true);
+
+            const response = await axios.post(`/api/courses/${courseId}/checkout`)
+
+            window.location.assign(response.data.url);
+        } catch {
+            toast.error("Something went wrong");
+        } finally {
+            setIsLoanidg(false);
+        }
+    }
+
     return(
         <Button
+        onClick={onClick}
+        disabled={isLoading}
+        size="sm"
         className="w-full md:w-auto"
         >
             Enroll for {formatPrice(price)}
